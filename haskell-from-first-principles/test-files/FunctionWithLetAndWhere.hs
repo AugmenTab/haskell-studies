4c6b6b0c2baeaa537e96e7e04e419f5e1cbd09a1
AugmenTab <tbaum92@live.com>
2021-02-23 12:07:36 -0700

    Completed chapter 02 exercises.

--- haskell-from-first-principles/test-files/FunctionWithLetAndWhere.hs
+++ haskell-from-first-principles/test-files/FunctionWithLetAndWhere.hs
@@ -9,3 +9,19 @@ printInc n = print plusTwo
 printInc2 n =
   let plusTwo = n + 2
   in print plusTwo
+
+
+rewrite1  = x * 3 + y
+  where x = 3
+        y = 1000
+
+
+rewrite2  = x * 5
+  where y = 10
+        x = 10 * 5 + y
+
+
+rewrite3  = z / x + y
+  where x = 7
+        y = negate x
+        z = y * 10
