5ea529382847e194823e8561897c8e30e79252c8
AugmenTab <tbaum92@live.com>
2021-08-15 09:45:16 -0600

    Completed chapter 2.

---
+++ learn-you-a-haskell/chapter02.hs
@@ -0,0 +1,18 @@
+-- chapter02
+module Chapter02 where
+
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+
+factorial :: Integer -> Integer
+factorial n = product [1..n]
+
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
